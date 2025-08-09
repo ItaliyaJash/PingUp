a870beb4f9a0359c330b41fba33e32fd393dd870
ItaliyaJash <italiyajash1505@gmail.com>
2025-08-09 19:04:07 +0530

    fix: update import paths for User model to include file extension

--- server/server.js
+++ server/server.js
@@ -3,14 +3,14 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js'
 import { inngest, functions } from './inngest/index.js' 
-import { serve } from 'inngest/express'; // Add this import for 'serve'
+import { serve } from 'inngest/express'; 
 
 const app = express();
 
 await connectDB();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors());    
 
 app.get('/', (req, res) => res.send('Server is running'))
 app.use('/api/inngest', serve({ client: inngest, functions }))
